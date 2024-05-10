ed633aaa4b3b17a5adb369bfe28a23cb92729ae7
vishal kumar verma <vv081997@gmail.com>
2024-05-10 16:12:25 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, importProvidersFrom } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent   {
   title = 'serve';
 }
